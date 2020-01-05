5a0a4cf1de2213114c50739d1a20e45b0d4dd7d9
Dave Nolan <dpn.nolan@gmail.com>
2020-01-04 22:49:02 -0500

    key presses added to array

--- client/src/components/game/index.js
+++ client/src/components/game/index.js
@@ -1,40 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import syntaxComponent from "../syntax";
 import htmlJson from "../../utils/html.json"
 import useKeyPress from '../../hooks/useKeyPress';
 
-
 function Game() {
   const[html, setHtml] = useState(htmlJson)
   const[index, setIndex] = useState(0)
   const[current, setCurrent] = useState(html[0])
-  const[myClass, setMyClass] = useState("word")
+  const[check, setCheck] = useState([])
 
-function resetAnimation(){
-  setMyClass("")
-  setMyClass("word")
-}
-const check = []
 
   useKeyPress(key => {
-
-    console.log(key, current.syntax)
     
+    console.log(key, current.syntax)
+    // console.log(check)
     if (key === current.syntax) {
-      current.syntax = ""
-      current.syntax = newIndex
-      let newIndex = index + 1
-      setIndex(newIndex);
-      setCurrent(htmlJson[index]);
-      resetAnimation();
+      setCheck([...check, key])
+      // let newIndex =  index + 1
+      // setIndex(newIndex);
+      // current.syntax = newIndex
+    //   setCurrent(htmlJson[index]);
+      // setTimeout(console.log("check: " + check),1000)
+  
     }
 });
-console.log("check: " + check.length)
+useEffect(()=> {console.log(check)},[check]) 
+
+
 
   return (
     <div className="gameDiv container">
-        <div className="word">
+        <div key={+new Date()} className="word">
         {/* {this.state.html.map(html=> ( */}
           <syntaxComponent>{current.syntax}</syntaxComponent>
           {/* ))} */}

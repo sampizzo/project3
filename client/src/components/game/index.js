389c47262f198930abeeaa672aecb06345d3690d
SP <sampizzo@gmail.com>
2020-01-05 22:00:36 -0500

    imported navbar

--- client/src/components/game/index.js
+++ client/src/components/game/index.js
@@ -6,6 +6,7 @@ import useKeyPress from "../../hooks/useKeyPress";
 import GameForm from "../gameForm/gameForm";
 import { PromiseProvider } from "mongoose";
 import ScoreBoard from "../scoreboard";
+import Navbar from "../Navbar/index.js";
 
 
 //UIFX Audio imports
@@ -80,6 +81,7 @@ function Game(props) {
 
   return (
     <div className="container">
+    <Navbar />
       <div className="gameDiv">
         <div key={+new Date(wordIndex)} className="word">
           {/* {this.state.html.map(html=> ( */}

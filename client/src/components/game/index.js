d476345f288d7df1b1426eecb64148727505e615
Dave Nolan <dpn.nolan@gmail.com>
2020-01-05 20:50:48 -0500

    added game styling, audioFX, and Level feature

--- client/src/components/game/index.js
+++ client/src/components/game/index.js
@@ -1,66 +1,91 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import syntaxComponent from "../syntax";
-import htmlJson from "../../utils/html.json"
-import useKeyPress from '../../hooks/useKeyPress';
-import GameForm from '../gameForm/gameForm';
+import htmlJson from "../../utils/html.json";
+import useKeyPress from "../../hooks/useKeyPress";
+import GameForm from "../gameForm/gameForm";
 import { PromiseProvider } from "mongoose";
-import ScoreBoard from "../scoreboard"
+import ScoreBoard from "../scoreboard";
 
+
+//UIFX Audio imports
+import UIfx from "uifx";
+import coinAudio from "./coin.mp3";
+import lvlAudio from "./lvl.mp3"
+const coin = new UIfx(coinAudio);
+const lvl = new UIfx(lvlAudio)
 
 
 function Game(props) {
-  const[index, setIndex] = useState(0)
-  const[word, setWord] = useState(htmlJson[0].syntax.split("").map(letter=> {return({char: letter, guessed: false})}))
-  const[wordIndex, setWordIndex] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [word, setWord] = useState(
+    htmlJson[0].syntax.split("").map(letter => {
+      return { char: letter, guessed: false };
+    })
+  );
+  const [wordIndex, setWordIndex] = useState(0);
   const [score, setScore] = useState(0);
+  const [lvl, setLvl] = useState(1);
 
   useKeyPress(key => {
-    console.log(key, word[index])
+    console.log(key, word[index]);
     // console.log(check)
     if (key === word[index].char) {
       word[index].guessed = true;
-      let newIndex =  index + 1
+      let newIndex = index + 1;
       if (newIndex === word.length) {
-        setScore(score + 10);
-        console.log("Score: " + score);
+        setScore(score + 100);
+        coin.play();
+        if (
+          score % 10 === 0
+        ) {
+          setLvl(lvl + 1);
+          lvl.play();
+        }
         setWordIndex(wordIndex + 1);
+      
       }
       setIndex(newIndex);
-      // current.syntax = newIndex
-      // setTimeout(console.log("check: " + check),1000)
-  
     }
-  
-});
+  });
 
-useEffect(()=> {
-  let newWord = htmlJson[wordIndex].syntax.split("").map(letter=> {return({char: letter, guessed: false})})
-  setWord(newWord)
-  setIndex(0)
+  useEffect(() => {
+    let newWord = htmlJson[wordIndex].syntax.split("").map(letter => {
+      return { char: letter, guessed: false };
+    });
+    setWord(newWord);
+    setIndex(0);
+  }, [wordIndex]);
 
-},[wordIndex]) 
-
-useEffect(()=> {
-  setScore(score + 10)
-},score)
-
-
+  useEffect(() => {
+    setScore(score);
+  }, score);
 
   return (
-    <div>
-    <div className="gameDiv container">
+    <div className="container">
+      <div className="gameDiv">
         <div key={+new Date(wordIndex)} className="word">
-        {/* {this.state.html.map(html=> ( */}
-          <syntaxComponent>{word.map(letter => <span className= {letter.guessed? "guessed" : "unguessed"}>{letter.char}</span>)}</syntaxComponent>
+          {/* {this.state.html.map(html=> ( */}
+          <syntaxComponent>
+            {word.map(letter => (
+              <span className={letter.guessed ? "guessed" : "unguessed"}>
+                {letter.char}
+              </span>
+            ))}
+          </syntaxComponent>
           {/* ))} */}
         </div>
-    </div>
-      <GameForm />
-      <ScoreBoard />
+      </div>
+      {/* <GameForm /> */}
+      <div>
+        <div className="scoreBoard">
+          <p>
+            Coins: {score} Level: {lvl}
+          </p>
+        </div>
+      </div>
     </div>
   );
-
-  }
+}
 
 export default Game;

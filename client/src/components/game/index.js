e4b3a1e86bd9e8c8bf8d368e15e418d1a8d40fc1
Dave Nolan <dpn.nolan@gmail.com>
2020-01-05 21:09:53 -0500

    added life hook

--- client/src/components/game/index.js
+++ client/src/components/game/index.js
@@ -11,9 +11,20 @@ import ScoreBoard from "../scoreboard";
 //UIFX Audio imports
 import UIfx from "uifx";
 import coinAudio from "./coin.mp3";
+import themeAudio from "./Birabuto-Kingdom.mp3"
 import lvlAudio from "./lvl.mp3"
-const coin = new UIfx(coinAudio);
+const coin = new UIfx(coinAudio, 
+  {
+    volume: 0.4, // number between 0.0 ~ 1.0
+    throttleMs: 50
+  });
+  const theme = new UIfx(themeAudio, 
+    {
+      volume: 0.4, // number between 0.0 ~ 1.0
+      throttleMs: 50
+    });
 const lvl = new UIfx(lvlAudio)
+theme.play();
 
 
 function Game(props) {
@@ -26,6 +37,8 @@ function Game(props) {
   const [wordIndex, setWordIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [lvl, setLvl] = useState(1);
+  const [lives, setLives] = useState(3);
+
 
   useKeyPress(key => {
     console.log(key, word[index]);
@@ -40,10 +53,14 @@ function Game(props) {
           score % 10 === 0
         ) {
           setLvl(lvl + 1);
-          lvl.play();
         }
         setWordIndex(wordIndex + 1);
-      
+      }
+      else if (newIndex ==! word.length) {
+        setIndex(lives - 1)
+        if (lives === -1) {
+          //game over
+        }
       }
       setIndex(newIndex);
     }
@@ -79,9 +96,9 @@ function Game(props) {
       {/* <GameForm /> */}
       <div>
         <div className="scoreBoard">
-          <p>
-            Coins: {score} Level: {lvl}
-          </p>
+          <h2>
+            Coins: {score} Level: {lvl} Lives: {lives}
+          </h2>
         </div>
       </div>
     </div>

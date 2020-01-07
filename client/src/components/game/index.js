85df0e0a6fbce163b60898f534829e0ffc3655ea
Dave Nolan <dpn.nolan@gmail.com>
2020-01-07 12:57:49 -0500

    gameover screen loading

--- client/src/components/game/index.js
+++ client/src/components/game/index.js
@@ -8,13 +8,16 @@ import ScoreBoard from "../scoreboard";
 import Navbar from "../Navbar/index.js";
 import { Redirect } from "react-router-dom";
 
-//Gifs and images
-import flagpole from "./flagpole.gif";
-import coinGif from "./coin.gif";
-import dieGif from "./dies.gif";
-import extraLife from "./1up.png";
+                        //Gifs and images
+///////////////////////////////////////////////////////////////////
+// import flagpole from "./flagpole.gif";
+// import coinGif from "./coin.gif";
+// import dieGif from "./dies.gif";
+// import extraLife from "./1up.png";
 
-//UIFX Audio imports
+                          //UIFX Audio imports
+///////////////////////////////////////////////////////////////////
+
 import UIfx from "uifx";
 import coinAudio from "./coin.mp3";
 import themeAudio from "./Birabuto-Kingdom.mp3";
@@ -36,6 +39,14 @@ const life = new UIfx(lifeAudio, {
 const lvl = new UIfx(lvlAudio);
 theme.play();
 
+
+
+///////////////////////////////////////////////////////////////////
+
+                      //GAME FUNCTION BEGIN
+
+///////////////////////////////////////////////////////////////////
+
 function Game(props) {
   const [index, setIndex] = useState(0);
   const [word, setWord] = useState(
@@ -48,60 +59,73 @@ function Game(props) {
   const [lvl, setLvl] = useState(1);
   const [lives, setLives] = useState(3);
   const [mistakes, setMistakes] = useState(0);
-  // const [show, setShow] = useState(false);
-  // const [gif, setGif] = useState(none);
+  const [show, setShow] = useState(false);
   let [counter, setCounter] = useState(10);
-  let decrement;
- 
-  console.log(counter);
+  const [coinsCollected, setCoinsCollected] = useState(0);
+  const [wordCount, setWordCount] = useState(0);
+  const [errCount, setErrCount] = useState(0);
+
+
+
+
+            //TIMER FUNCTION, IF COUNTER = 0, -1 LIVES
+///////////////////////////////////////////////////////////////////
 
   useEffect(() => {
     let timer = setInterval(() => {
-      setCounter( counter => counter - 1);
+      setCounter(counter => counter - 1);
     }, 1000);
   }, []);
-  if (counter == 0) {
+  if (counter === 0) {
     setWordIndex(wordIndex + 1);
     setLives(lives - 1);
     life.play();
     setCounter(10);
+    if (lives === 1) {
+      console.log("lives = 0")
+      setShow(true);
+      console.log(show)
+    }
   }
-  useKeyPress(key => {
-    // console.log(key, word[index]);
+
+
+                        //KEY PRESS HOOK
+///////////////////////////////////////////////////////////////////
+useKeyPress(key => {
     if (key === word[index].char) {
       word[index].guessed = true;
       let newIndex = index + 1;
+      setScore(score + 10);
       if (newIndex === word.length) {
-        setScore(score + 100);
+        setCoinsCollected(coinsCollected + 1)
+        setWordCount(wordCount + 1)
         coin.play();
         setCounter(10);
-        if (score % 10 === 0) {
+        if (score % 100 === 0) {
           setLvl(lvl + 1);
         }
         setWordIndex(wordIndex + 1);
-      }
-
-      //   // if (lives === 0) {
-      //   //   return <Redirect to="/bonus" />;
-      //   //   //game over
-      //   // }
-      // }
+      } 
       setIndex(newIndex);
-    }
-    if (word.animationEnd) {
-      setLives(lives - 1);
-      setWordIndex(wordIndex + 1);
     }
     if (key !== word[index].char) {
       setMistakes(mistakes + 1);
+      setErrCount(errCount + 1)
       if (mistakes === 3) {
         setLives(lives - 1);
         setMistakes(0);
         life.play();
+        if (lives === 1) {
+          console.log("lives = 0")
+          setShow(true);
+          console.log(show)
+        }
       }
     }
   });
 
+                    //CHANGE CSS OF LETTER TYPED
+///////////////////////////////////////////////////////////////////
   useEffect(() => {
     let newWord = htmlJson[wordIndex].syntax.split("").map(letter => {
       return { char: letter, guessed: false };
@@ -110,40 +134,42 @@ function Game(props) {
     setIndex(0);
   }, [wordIndex]);
 
-  // useEffect(() => {
-  //   setScore(score);
-  // }, score);
 
+
+
+                          //RETURN OF THE JEDI
+///////////////////////////////////////////////////////////////////
   return (
     <div className="container">
-      <Navbar />
-      <div></div>
-      <div className="gameDiv">
-        <div id="lvl" className="gif">
-          <img src={flagpole}></img>
+      {show === true && <ScoreBoard lvl={lvl} score={score} coinsCollected={coinsCollected} wordCount={wordCount} errCount={errCount} />}
+      <div id="Game" style={ show ? { display: "none" }  : { display: "block" } }>
+        <div className="gameDiv">
+          {/* <div id="lvl" className="gif">
+            <img src={flagpole}></img>
+          </div>
+          <div id="coin" className="gif">
+            <img src={coinGif}></img>
+          </div>
+          <div id="die" className="gif">
+            <img src={dieGif}></img>
+          </div> */}
+          <div key={+new Date(wordIndex)} className="word">
+            <syntaxComponent>
+              {word.map(letter => (
+                <span className={letter.guessed ? "guessed" : "unguessed"}>
+                  {letter.char}
+                </span>
+              ))}
+            </syntaxComponent>
+          </div>
         </div>
-        <div id="coin" className="gif">
-          <img src={coinGif}></img>
-        </div>
-        <div id="die" className="gif">
-          <img src={dieGif}></img>
-        </div>
-        <div key={+new Date(wordIndex)} className="word">
-          <syntaxComponent>
-            {word.map(letter => (
-              <span className={letter.guessed ? "guessed" : "unguessed"}>
-                {letter.char}
-              </span>
-            ))}
-          </syntaxComponent>
-        </div>
-      </div>
-      <div>
-        <div className="scoreBoard">
-          <h2>
-            Coins: {score} Level: {lvl} Lives: {lives} Mistakes: {mistakes}{" "}
-            Time: {counter}
-          </h2>
+        <div>
+          <div className="scoreBoard">
+            <h2>
+              Score: {score} Coins: {coinsCollected} Level: {lvl} Lives: {lives} Mistakes: {mistakes}{" "} 
+              Time: {counter}
+            </h2>
+          </div>
         </div>
       </div>
     </div>
@@ -151,5 +177,3 @@ function Game(props) {
 }
 
 export default Game;
-
-
--- client/src/components/scoreboard/index.js
+++ client/src/components/scoreboard/index.js
@@ -1,24 +1,25 @@
 import React from "react";
-
+import "./style.css"
 // By extending the React.Component class, Counter inherits functionality from it
 class ScoreBoard extends React.Component {
   // Setting the initial state of the Counter component
-  state = {
-    score: 0
-  };
 
-  // handleIncrement increments this.state.count by 1
-  handleIncrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ score: this.state.score + 10 });
-  };
 
   // The render method returns the JSX that should be rendered
   render() {
     return (
       <div>
-        <div className="card-body">
-          <p>Score: {this.state.score}</p>
+        <div className="gameOver">
+          <h1>Game Over</h1>
+          <hr />
+          <h2>Level: {this.props.lvl}</h2>
+          <h2>Highscore: {this.props.score}</h2>
+          <h2>Coins Collected:  {this.props.coinsCollected}</h2>
+          <h2>Total Words: {this.props.wordCount}</h2>
+          <h2>Error Count: {this.props.errCount}</h2>
+          <hr />
+          <button className="button">Return To Menu</button>
+          <button className="button">Replay</button>
         </div>
       </div>
     );

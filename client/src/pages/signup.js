e071fa65523629fcbdc6cfdd7c8b68faa95767c1
Dave Nolan <dpn.nolan@gmail.com>
2020-01-06 23:13:41 -0500

    fixed capitalization of imports

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Signup from "../components/Signup";
+import Signup from "../components/signup";
 
 class SignupPage extends Component {
   render() {

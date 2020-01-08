b17bb31b237b2e9d4a41e1e3461cc6fd2b0e939c
Sabrina <sabrina.muniz@icloud.com>
2020-01-07 21:28:11 -0500

    created user page, logout button and function, changed routes on app.js to show when user is logged in

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,62 @@ import LoginPage from "./pages/login";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"
 import BonusPage from "./pages/bonus";
+import UserPage from "./pages/user";
+import Axios from "axios";
 
-
-function App() {
-  return (
-    <Router>
-      <Navbar></Navbar>
-      <div>
-        <Switch>
-          <Route exact path="/signup" component={SignUpPage} /> 
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/game" component={GamePage} />
-          <Route exact path="/bonus" component={BonusPage} />
-          </Switch>
-      </div>
-      <Footer></Footer>
-    </Router>
-  );
+class App extends Component {
+  state = {
+    userId: null,
+    loggedIn: false
+  }
+  componentDidMount (){
+    console.log("about to check user")
+    Axios.get("/getuser").then( (data) => {
+      console.log(data)
+      if(data.data._id){
+        this.setState({
+          userId: data.data._id,
+          loggedIn: true
+        })
+      }
+    })
+  }
+  render (){
+    console.log("state in app.js", this.state)
+    if (this.state.loggedIn){
+      return(
+        <Router>
+        <Navbar></Navbar>
+        <div>
+          <Switch>
+            <Route exact path="/signup" component={SignUpPage} /> 
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/user" component={UserPage} />
+            <Route exact path="/game" component={GamePage} />
+            <Route exact path="/bonus" component={BonusPage} />
+            </Switch>
+        </div>
+        <Footer></Footer>
+      </Router>  
+      )
+    }
+    else{
+      return (
+        <Router>
+        <Navbar></Navbar>
+        <div>
+          <Switch>
+            <Route exact path="/signup" component={SignUpPage} /> 
+            <Route exact path="/login" component={LoginPage} />
+            </Switch>
+        </div>
+        <Footer></Footer>
+      </Router>  
+        );
+    }
+  
 }
+}
+
 
 export default App;

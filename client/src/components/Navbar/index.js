c26eafdfbdb4674b060fdda3600b82ea6306b3bc
Sabrina <sabrina.muniz@icloud.com>
2020-01-08 22:14:29 -0500

    syntax tutor link on navbar to menu page

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -35,7 +35,7 @@ class Navbar extends React.Component {
     // } else {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/menu">
           Syntax Tutor
       </Link>
         <div>
